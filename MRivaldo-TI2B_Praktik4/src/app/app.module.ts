b572be0beb8291f76ec6c454f0b15cac0456e6b1
M Rivaldo <rivaldzsetyo@gmail.com>
2019-02-20 20:12:26 +0700

    template bagian 2
    template bagian 2

--- MRivaldo-TI2B_Praktik4/src/app/app.module.ts
+++ MRivaldo-TI2B_Praktik4/src/app/app.module.ts
@@ -4,13 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseComponent } from './course/course.component';
+//import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
-    CourseComponent
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
--- MRivaldo-TI2B_Praktik4/src/app/courses/courses.component.ts
+++ MRivaldo-TI2B_Praktik4/src/app/courses/courses.component.ts
@@ -8,31 +8,14 @@ import { CoursesService } from '../courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  nama = 'M Rivaldo S';
-  alamat = 'dusun Bogo';
-  tanggal = new Date(2019, 1, 10);
-
-  Courses;
-  constructor(private service:CoursesService){
-    this.Courses=service.getCourses();
+  title = 'Belajar angular';
+  getTitle(){
+    return this.title;
   }
-  // Courses = [
-    
-  //   {id: 1, name: 'Berlari', ket: 'lari 1 jam tiap hari'},
-  //   {id: 2, name: 'Senam', ket:'senam tiap sore'},
-  //   {id: 3, name: 'Bermain Musik', ket:'bermain musik tiap waktu'},
-    
-  // ];
 
-  //constructor() { }
-
+  constructor(){  }
+  
   ngOnInit() {
-  }
-  getNama(){
-    return this.nama;
-  }
-  getAlamat(){
-    return this.alamat;
   }
 
 }
