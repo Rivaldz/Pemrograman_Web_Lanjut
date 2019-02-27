78e0a186c4ca1626828933e5c095c43e1a9c1325
M Rivaldo <rivaldzsetyo@gmail.com>
2019-02-27 20:11:14 +0700

    saya mulai dari awal lagi

--- MRivaldo-TI2B_Praktik3/ts-hello/main.js
+++
@@ -1,9 +0,0 @@
-var log = function pesan() {
-    console.log(pesan);
-};
-//code diatas bisa disingkat menggunakan function arrow 
-var dolog = function (pesan) {
-    console.log(pesan);
-};
-//atau seperti berikut 
-var dolog1 = function (pesan) { return console.log(pesan); };
