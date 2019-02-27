d4c0a339d139ee811c19cafca6ab1160b2ea2a9f
M Rivaldo <rivaldzsetyo@gmail.com>
2019-02-27 23:17:20 +0700

    Menyelesaiakn jpbsheet 2

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,59 @@
-function log(message) {
-    console.log(message);
-}
-var message = 'hello Word';
-log(message);
+// function log(message){
+//     console.log(message);
+// }
+// let message = 'hello Word';
+// log(message);
+// function doSomething(){
+//     for(let i = 0 ; i < 5 ; i++){
+//         console.log(i);
+//     }
+//     console.log('Finaly : ' + i); 
+// }
+// doSomething();
+// let count = 5;
+// count = 'a';
+// let pesan ;
+// pesan 'abcd';
+// let percobaan=(<string>pesan).endsWith('c');
+// let alternative=(pesan as string).endsWith('c');
+// class Pointku{
+//     a:number;
+//     b:number;
+//     draw(){
+//         //....
+//     }
+//     getDistance(another:Pointku){
+//         //....
+//     }
+// }
+// let log = function pesan(){
+//     console.log(pesan);
+// }
+// let dialog=(pesan)=>{
+//     console.log(pesan);
+// }
+// let dialog1=(pesan)=>console.log(pesan);
+// interface Point{
+//     x:number;
+//     y:number;
+// }
+// let drawPoint=(point:Point)=>{
+//     //..
+// }
+// drawPoint({
+//     x:1,
+//     x:3,
+// })
+var Pointku1 = /** @class */ (function () {
+    function Pointku1() {
+    }
+    Pointku1.prototype.draw = function () {
+        console.log('X' + this.a + ',Y' + this.b);
+    };
+    Pointku1.prototype.getDistance = function (another) {
+        //...
+    };
+    return Pointku1;
+}());
+var coins;
+coins.draw();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,7 +1,74 @@
-function log(message){
-    console.log(message);
+// function log(message){
+//     console.log(message);
+// }
+
+// let message = 'hello Word';
+
+// log(message);
+
+// function doSomething(){
+//     for(let i = 0 ; i < 5 ; i++){
+//         console.log(i);
+//     }
+//     console.log('Finaly : ' + i); 
+// }
+// doSomething();
+
+// let count = 5;
+// count = 'a';
+
+// let pesan ;
+// pesan 'abcd';
+// let percobaan=(<string>pesan).endsWith('c');
+// let alternative=(pesan as string).endsWith('c');
+
+// class Pointku{
+//     a:number;
+//     b:number;
+
+//     draw(){
+//         //....
+//     }
+
+//     getDistance(another:Pointku){
+//         //....
+//     }
+// }
+
+// let log = function pesan(){
+//     console.log(pesan);
+// }
+
+// let dialog=(pesan)=>{
+//     console.log(pesan);
+// }
+// let dialog1=(pesan)=>console.log(pesan);
+
+// interface Point{
+//     x:number;
+//     y:number;
+
+// }
+// let drawPoint=(point:Point)=>{
+//     //..
+// }
+
+// drawPoint({
+//     x:1,
+//     x:3,
+// })
+class Pointku1{
+    a:number;
+    b:number;
+
+    draw(){
+        console.log('X' + this.a + ',Y' + this.b);
+    }
+    getDistance(another:Pointku1){
+        //...
+    }
+
 }
 
-let message = 'hello Word';
-
-log(message);
+let coins:Pointku1
+coins.draw();
