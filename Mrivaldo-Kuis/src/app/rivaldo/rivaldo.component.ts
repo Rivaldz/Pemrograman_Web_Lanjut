bec0b7a227cefe460994a34b72421d440f570638
Rivaldz <rivalzsetyo@gmail.com>
2019-03-07 14:05:47 +0000

    try finish kuis

--- Mrivaldo-Kuis/src/app/rivaldo/rivaldo.component.ts
+++ Mrivaldo-Kuis/src/app/rivaldo/rivaldo.component.ts
@@ -3,23 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rivaldo',
   templateUrl: './rivaldo.component.html',
-  styleUrls: ['./rivaldo.component.css']
+  // template: `<input [(ngModel)]="name" type="text"> 
+  //  {{name}}`,
+  styles:[]
 })
 export class RivaldoComponent implements OnInit {
 
 
   title = 'MUHAMMAD RIVALDO SETYO PURNOMO'
   title2= 'Menggunakan stringinterpolar(databinding)'
-  alamat = 'Jalan imam bonjol no 23 pagar hitam'
-
-  onSavee(){
-    console.log('alamat jl senggani dusun bogo desa pelem');
+  
+  public name="";
+  public alamat="";
+  
+  onClick(){
+    this.alamat='Coding tak perlu di hapal ker cukup di pahami okeyy';
   }
-
-
-
   constructor() { }
-
 
   ngOnInit() {
   }
