4b77d68f7070d43264ff2d09575dde53d41871ed
Rivaldz <rivalzsetyo@gmail.com>
2019-03-12 23:35:20 +0000

    menambahkan bootstrap dsb

--- MRivaldo-TI2B_Bab4-Components/src/app/app.module.ts
+++ MRivaldo-TI2B_Bab4-Components/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { FavoritComponent } from './favorit/favorit.component';
+import { AngularFontAwesomeModule} from 'angular-font-awesome';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { FavoritComponent } from './favorit/favorit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
