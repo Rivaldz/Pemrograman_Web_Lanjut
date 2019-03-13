c1d1ce46f094d67e6bfe3b07c24276066ba5ec44
M Rivaldo <rivaldzsetyo@gmail.com>
2019-03-13 14:36:13 +0700

    Membuat ngFor

--- Mrivaldo-TI2B_Bab5-Directive/src/app/app.component.ts
+++ Mrivaldo-TI2B_Bab5-Directive/src/app/app.component.ts
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Mrivaldo-TI2B';
   courses=[1,2];
+
+  viewMode='map';
+
+  coursesFor=[
+    {id:1,name:'courses1'},
+    {id:2,name:'courses2'},
+    {id:3,name:'courses3'},
+    {id:4,name:'courses4'},
+    {id:5,name:'courses5'},
+  ]
 }
