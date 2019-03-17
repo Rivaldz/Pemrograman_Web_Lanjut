8db82c8ceeb841303bd5a162e30f9fe1cd9edd61
Rivaldz <rivalzsetyo@gmail.com>
2019-03-17 21:59:18 +0000

    Menambahkan dekorator apabila di tab akan muncul huruf besar

--- Mrivaldo-TI2B_Bab5-Directive/src/app/input-format.directive.ts
+++ Mrivaldo-TI2B_Bab5-Directive/src/app/input-format.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input, ElementRef } from '@angular/core';
+import { format } from 'url';
 
 @Directive({
   selector: '[appInputFormat]'
 })
 export class InputFormatDirective {
 
-  constructor() { }
+  @Input('appInputFormat')format;
+
+  // @HostListener('focus') onfocus(){
+  //   console.log("onFocus")
+  // }
+
+  constructor(private el:ElementRef) { }
+
+  @HostListener('blur')onblur(){
+  //  console.log("onBlur")
+  let value:string = this.el.nativeElement.value;
+  if(this.format=='lowercse')
+    this.el.nativeElement.value = value.toLowerCase();
+  
+  else
+    this.el.nativeElement.value = value.toUpperCase();
+  }
 
 }
