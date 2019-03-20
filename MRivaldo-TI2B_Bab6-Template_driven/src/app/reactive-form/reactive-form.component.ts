3794ff95390aa5c90a8dc22c42e96e96443b964e
Rivaldz <rivalzsetyo@gmail.com>
2019-03-20 16:31:58 +0000

    Modifikasi code in reactive-form.component.html

--- MRivaldo-TI2B_Bab6-Template_driven/src/app/reactive-form/reactive-form.component.ts
+++ MRivaldo-TI2B_Bab6-Template_driven/src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {mahasiswa} from './../mahasiswa.interface';
-import {FormGroup, FormBbuilder, FormBuilder} from '@angular/forms';
+import {FormGroup,FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
