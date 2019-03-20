bfa40d63e243e75daf0a73e24d424fa35b6d103a
Rivaldz <rivalzsetyo@gmail.com>
2019-03-20 14:49:07 +0000

    Membuat componen  reactive-form

--- MRivaldo-TI2B_Bab6-Template_driven/src/app/app.module.ts
+++ MRivaldo-TI2B_Bab6-Template_driven/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    ReactiveFormComponent
   ],
   imports: [
     BrowserModule,
