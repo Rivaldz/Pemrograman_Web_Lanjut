86b9fc8dc6927d78506f897c70707435b9785289
Rivaldz <rivalzsetyo@gmail.com>
2019-03-20 14:51:14 +0000

    membuat komponen dengan nama template-driven

--- MRivaldo-TI2B_Bab6-Template_driven/src/app/app.module.ts
+++ MRivaldo-TI2B_Bab6-Template_driven/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    ReactiveFormComponent
+    ReactiveFormComponent,
+    TemplateDrivenComponent
   ],
   imports: [
     BrowserModule,
