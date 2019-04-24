ddbdc26c30d57ddf6bb6dc865af0700716e3c79c
Rivaldo Setyo <rivaldosetyo@Rivaldos-MacBook-Pro.local>
2019-04-24 13:41:38 +0700

    import http service

--- MRivaldo-TI2B-HTTPServies/src/app/app.module.ts
+++ MRivaldo-TI2B-HTTPServies/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
