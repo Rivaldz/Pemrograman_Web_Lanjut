69282417011f4da966ec79a3b8fc2a47f67d5f9d
Rivaldo Setyo <rivaldosetyo@Rivaldos-MacBook-Pro.local>
2019-05-08 12:23:43 +0700

    Menambahkan coding html di navbar.component.html

--- MRivaldo-TI2B_JS10-RoutingDanNavigasi/src/app/app.module.ts
+++ MRivaldo-TI2B_JS10-RoutingDanNavigasi/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostComponentComponent } from './post-component/post-component.component';
@@ -20,8 +23,16 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'form',component:FormMemberComponent},
+      {path:'post',component:PostComponentComponent},
+      {path:'**',component:NotFoundComponent}])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
